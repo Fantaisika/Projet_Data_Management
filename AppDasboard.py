d95da87c67a14e47f91aeda7a4abdc2189fa3af2
Atji00 <atjicheick@gmail.com>
2024-12-14 12:03:35 +0100

    modification en cours

--- AppDasboard.py
+++ AppDasboard.py
@@ -267,10 +267,49 @@ def Tableau_Bord():
   col2.write(f"Ville d'habitation: {ville}")
   col2.write(f"Adresse: {adresse}")
   
-  #col3.subheader(f"{emetteur}")
   col3.markdown(f"<h3 style='font-size:20px;'>{emetteur}</h3>", unsafe_allow_html=True)
   col3.image(carteimage)
   col3.write(f"Carte: {carte}")
+
+# Construction des graphs
+  st.subheader("Suivi des indicateurs sur les transactions", divider='blue')
+  persondata['date'] = pd.to_datetime(persondata["date"]).dt.date
+  date_min = persondata['date'].min()
+  date_max = persondata['date'].max()
+
+  col1, col2 = st.columns(2)
+  debut = col1.date_input("selectionner date debut", date_min,
+                          min_value = date_min, max_value = date_max)
+  fin = col2.date_input("selectionner date fin", date_max,
+                          min_value = date_min, max_value = date_max)
+  persondata_filtre = (persondata[(persondata['date'] >= debut) & 
+                          (persondata['date'] <= fin)])
+  transnumb = persondata_filtre['daily_transactions'].sum()
+  transamount = persondata_filtre['daily_amount$'].sum()
+  fraudnum = persondata_filtre['daily_number_fraud'].sum()
+  fraudamount = persondata_filtre['daily_fraud_amount$'].sum()
+  
+  col1,col2,col3 = st.columns(3)
+  col1.markdown(f"<h2 style='font-size:15px, color: darkblue;'>Periode: {date_min} au {date_max}:</h2>",
+                unsafe_allow_html=True)
+  col2.markdown(f"<h2 style='font-size:15px, color: darkblue;'>Nombre de transactions{space*6}{transnumb}</h2>",
+                unsafe_allow_html=True)
+  col1.markdown(f"<h2 style='font-size:15px, color: darkblue;'>Periode: {date_min} au {date_max}:</h2>",
+                unsafe_allow_html=True)
+  col3.write(f"Total des montants{space*6}{transamount} USD")
+
+  col1, col2 = st.columns(2)
+  col1.markdown(f"<h2 style='font-size:15px;'>Evolution des montants sur la période:</h2>",
+                unsafe_allow_html=True)
+  col1.area_chart(persondata_filtre[['date','daily_amount$']],
+                  x="date", y="daily_amount$", x_label= 'Periode',
+                  y_label='Montant USD')
+
+  col2.markdown(f"<h2 style='font-size:15px;'>Evolution des transactions sur la période:</h2>",
+                    unsafe_allow_html=True)
+  col2.bar_chart(persondata_filtre[['date','daily_transactions']],
+                 y="daily_transactions", x="date", x_label='Periode',
+                 y_label='Volume des transactions')
 
 def Geodata():
   st.title("Bienvenue chez SDA Analytics Group")
